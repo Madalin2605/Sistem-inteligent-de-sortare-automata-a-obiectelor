bcffc712a32cea569bf75502eaf1886cdef51edb
Madalin2605 <101002001+Madalin2605@users.noreply.github.com>
2024-09-08 14:26:53 +0300

    Add files via upload

---
+++ YOLO/YOLOv8/yolov8.py
@@ -0,0 +1,8 @@
+from ultralytics.models.yolo import YOLO
+import torch
+
+if __name__ == '__main__':
+
+    model = YOLO('yolov8n.pt')
+
+    results = model.train(data='D:/YOLO/dataset_yolo/data.yaml', epochs=25, batch=4)
